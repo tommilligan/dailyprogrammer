a142c673e5d236f333f648fd9db76cabf383d0ef
Tom Milligan <tom@tommilligan.net>
2017-08-27 20:44:08 +0100

    Added timing and notes

--- dailyprogrammer/__main__.py
+++ dailyprogrammer/__main__.py
@@ -7,13 +7,17 @@ import argparse
 import importlib
 import logging
 import sys
+import time
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 def mainParser():
     """
     Command line parser
     """
     parser = argparse.ArgumentParser("dailyprogrammer working solutions")
-    parser.add_argument("-v", "--verbose", help="Turn logging up to debug")
+    parser.add_argument("-v", "--verbose", action="count", help="Turn logging")
     parser.add_argument("challenge", help="Challenge id. Matches the regex 'c\d{8}[hme]\d+' and style cYYYYMMDD<level><serial>")
     parser.add_argument("input", nargs="?", default=None, help="Challenge input. Defaults to stdin")
     return parser
@@ -23,7 +27,7 @@ def main():
     Command line entry point
     """
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch.setLevel(logging.WARN)
     ch.setFormatter(logging.Formatter("%(asctime)s|%(levelname)s|%(name)s|%(message)s"))
     root = logging.getLogger()
     root.handlers = [ch]
@@ -31,8 +35,12 @@ def main():
     parser = mainParser()
     args = parser.parse_args()
 
-    if args.verbose:
+    if args.verbose == 1:
+        ch.setLevel(logging.INFO)
+    elif args.verbose == 2:
         ch.setLevel(logging.DEBUG)
+
+    logger.debug(args)
 
     if args.input is None:
         challengeInput = sys.stdin.read()
@@ -42,7 +50,12 @@ def main():
     challengeModule = "dailyprogrammer.{0}".format(args.challenge)
     challenge = importlib.import_module(challengeModule)
 
+    t0 = time.time()
     result = challenge.main(challengeInput)
+    t1 = time.time()
+    elapsed = t1 - t0
+    logger.warn("challenge ran in %0.3f s" % elapsed)
+
     print(result)
 
 if __name__ == "__main__":
---
+++ notes/c20170811h0/notes.md
@@ -0,0 +1,25 @@
+# [2017-08-11] Challenge #326 [Hard] Multifaceted alphabet blocks
+
+https://www.reddit.com/r/dailyprogrammer/comments/6t0zua/20170811_challenge_326_hard_multifaceted_alphabet/
+
+### TL;DR
+
+Given a list of words, generate a series of alphabet blocks that can be used to spell all words.
+
+The first block has 1 face, the second 2, etc.
+
+How few blocks do you need, and what is written on each face?
+
+## Input
+
+https://pastebin.com/trMz6nWQ
+
+## Output
+
+see `output.txt`
+
+ran in 17.448 s
+
+## Checker
+
+http://erantapaa.github.io/daily-programmer/326/verify.html
