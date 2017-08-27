0c0c91a193119404f60e351c871544bbcf8ddccd
Tom Milligan <tom@tommilligan.net>
2017-08-27 16:44:43 +0100

    Brock and Bricks framework in place

--- dailyprogrammer/c20170811h0/__init__.py
+++ dailyprogrammer/c20170811h0/__init__.py
@@ -6,6 +6,83 @@ https://www.reddit.com/r/dailyprogrammer/comments/6t0zua/20170811_challenge_326_
 
 import collections
 
+class BrickException(Exception):
+    pass
+
+class Brick(object):
+    """
+    An alphabet brick with variable number of faces
+
+    :param int faces: Number of faces
+    """
+    def __init__(self, faces):
+        self.faces = faces
+        self.letters = []
+
+    def __repr__(self):
+        return repr(self.letters)
+
+    def contains(self, letter):
+        """
+        Returns whether the brick currently contains a letter
+
+        :param string letter: Letter to check
+        """
+        return letter in self.letters
+
+    def add(self, letter):
+        """
+        Add a letter to a face on the brick
+
+        If no faces are available or the letter already exists, ``BrickException`` is raised
+
+        :param string letter: Letter to add to the brick face
+        :raises: BrickException
+        """
+        if len(self.letters) >= self.faces:
+            raise BrickException("No free faces remaining")
+        elif self.contains(letter):
+            raise BrickException("Brick already contains '{0}'".format(letter))
+        else:
+            self.letters.append(letter)
+        
+    def remove(self, letter):
+        """
+        Remove a letter from the brick
+
+        :param string letter: Letter to remove from the brick face
+        :raises ValueError:
+        """
+        self.letters.remove(letter)
+
+    def replace(self, oldLetter, newLetter):
+        self.remove(oldLetter)
+        self.add(newLetter)
+
+class Bricks(object):
+    """
+    A series of alphabet bricks
+    """
+    def __init__(self):
+        self.bricks = []
+    
+    def __repr__(self):
+        return repr(self.bricks)
+
+    def __getitem__(self, key):
+        return self.bricks[key]
+
+    def add(self):
+        """
+        Add another brick to the series with N+1 faces
+
+        Returns the added brick
+        """
+        brick = Brick(len(self.bricks) + 1)
+        self.bricks.append(brick)
+        return brick
+
+
 def sortedDictValues(inputDict, reverse=False):
     """
     Returns the given dictionary as a list of keys sorted by value then key::
@@ -42,7 +119,7 @@ def wordsDecreasing(challengeInput):
     :param string challengeInput:
     :rtype: list
     """
-    words = [w.strip() for w in challengeInput.split("\n")]
+    words = [w.strip().upper() for w in challengeInput.split("\n")]
     wordLengths = {w: len(w) for w in words}
     wordsDecreasing = sortedDictValues(wordLengths, reverse=True)
     return wordsDecreasing
@@ -50,6 +127,21 @@ def wordsDecreasing(challengeInput):
 def main(challengeInput):
     words = wordsDecreasing(challengeInput)
     letters = lettersDecreasing("".join(words))
+
+    bricks = Bricks()
+
+    for word in words:
+        currentBrickIndex = 0
+        for letter in letters:
+            if letter in word:
+                try:
+                    currentBrick = bricks[currentBrickIndex]
+                except IndexError:
+                    currentBrick = bricks.add()
+                currentBrick.add(letter)
+                
+
+    print(bricks)
     return challengeInput
 
 if __name__ == "__main__":
--- test/test_c20170811h0/test___init__.py
+++ test/test_c20170811h0/test___init__.py
@@ -45,18 +45,90 @@ class TestLettersDecreasing(unittest.TestCase):
 class TestWordsDecreasing(unittest.TestCase):
     def testWord(self):
         testInput = "banana"
-        expected = ["banana"]
+        expected = ["BANANA"]
         actual = challenge.wordsDecreasing(testInput)
         self.assertEqual(actual, expected)
 
     def testWords(self):
         testInput = "spam\nbanana"
-        expected = ["banana", "spam"]
+        expected = ["BANANA", "SPAM"]
         actual = challenge.wordsDecreasing(testInput)
         self.assertEqual(actual, expected)
 
     def testWordsDeterministic(self):
         testInput = "eels\nspam\nbanana"
-        expected = ["banana", "spam", "eels"]
+        expected = ["BANANA", "SPAM", "EELS"]
         actual = challenge.wordsDecreasing(testInput)
         self.assertEqual(actual, expected)
+
+class TestBrick(unittest.TestCase):
+    def setUp(self):
+        self.brick = challenge.Brick(3)
+
+    def testDefault(self):
+        self.assertEqual(self.brick.letters, [])
+        self.assertEqual(self.brick.faces, 3)
+
+    def testAdd(self):
+        self.brick.add("F")
+        self.assertEqual(self.brick.letters, ["F"])
+
+    def testAddRepeat(self):
+        self.brick.add("F")
+        with self.assertRaises(challenge.BrickException):
+            self.brick.add("F")
+
+    def testRemove(self):
+        self.brick.add("F")
+        self.brick.add("A")
+        self.brick.remove("F")
+        self.assertEqual(self.brick.letters, ["A"])
+
+    def testRemoveInvalid(self):
+        with self.assertRaises(ValueError):
+            self.brick.remove("F")
+
+    def testReplace(self):
+        self.brick.add("F")
+        self.brick.replace("F", "A")
+        self.assertEqual(self.brick.letters, ["A"])
+
+    def testContainsTrue(self):
+        self.brick.add("F")
+        expected = True
+        actual = self.brick.contains("F")
+        self.assertEqual(actual, expected)
+
+    def testContainsFalse(self):
+        expected = False
+        actual = self.brick.contains("F")
+        self.assertEqual(actual, expected)
+
+    def testRepr(self):
+        for c in "CAT":
+            self.brick.add(c)
+        expected = "['C', 'A', 'T']"
+        self.assertEqual(str(self.brick), expected)
+
+class TestBricks(unittest.TestCase):
+    def setUp(self):
+        self.bricks = challenge.Bricks()
+    
+    def testDefault(self):
+        self.assertEqual(self.bricks.bricks, [])
+
+    def testAdd(self):
+        self.bricks.add()
+        self.assertEqual(len(self.bricks.bricks), 1)
+        self.assertEqual(self.bricks.bricks[0].faces, 1)
+
+    def testAddReturn(self):
+        actual = self.bricks.add()
+        self.assertEqual(actual.faces, 1)
+
+    def testGetBrick(self):
+        self.bricks.add()
+        expected = self.bricks.bricks[0]
+        actual = self.bricks[0]
+        self.assertEqual(actual, expected)
+
