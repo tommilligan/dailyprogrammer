2cbe6ac004842d935abbae48146ee06d868f155f
Tom Milligan <tom@tommilligan.net>
2017-09-07 18:28:34 +0100

    Fixed unused import

--- dailyprogrammer/challenges/c20170904e0/__init__.py
+++ dailyprogrammer/challenges/c20170904e0/__init__.py
@@ -4,7 +4,7 @@
 https://www.reddit.com/r/dailyprogrammer/comments/6y19v2/20170904_challenge_330_easy_surround_the_circles/
 """
 
-from dailyprogrammer.utils.logging import moduleLogger, objectLogger
+from dailyprogrammer.utils.logging import moduleLogger
 
 logger = moduleLogger(__name__)
 
